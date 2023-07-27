fd371846c1f15b58a31776b78e749293439c2e74
WesleyLiaw <wesleyliaw777@gmail.com>
2023-07-27 10:32:20 -0500

    added logo and splash screen

--- App.tsx
+++ App.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import {WebView} from 'react-native-webview';
+import {WithSplashScreen} from './components/Splash';
 
-const PEER_PREP = 'https://ecme.acep.org/diweb/assessment/self/home/self-eid/6508050/self-sid/163563942'
+const PEER_PREP =
+  'https://ecme.acep.org/diweb/assessment/self/home/self-eid/6508050/self-sid/163563942';
 
 export default function App() {
+  const [isAppReady, setIsAppReady] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      
+    }, 2000);
+    setIsAppReady(true);
+  }, []);
   return (
-    <View style={styles.container}>
-      <View style={{width: '100%', height: '100%'}}>
-        <WebView source={{uri: PEER_PREP}} />
+    <WithSplashScreen isAppReady={isAppReady}>
+      <View style={styles.container}>
+        <View style={{width: '100%', height: '100%'}}>
+          <WebView source={{uri: PEER_PREP}} />
+        </View>
       </View>
-    </View>
+    </WithSplashScreen>
   );
 }
 
