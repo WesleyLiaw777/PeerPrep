990695ad468882961aa4efe08dd664316abf6c07
WesleyLiaw <wesleyliaw777@gmail.com>
2023-08-16 12:02:37 -0500

    added js to avoid redirect to chrome

--- App.tsx
+++ App.tsx
@@ -1,24 +1,48 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import {ScrollView, StyleSheet, Text, View} from 'react-native';
-import {WebView} from 'react-native-webview';
+import WebView, {WebViewMessageEvent} from 'react-native-webview';
 import {WithSplashScreen} from './components/Splash';
 
-const PEER_PREP =
-  'https://ecme.acep.org/diweb/assessment/self/home/self-eid/6508050/self-sid/163563942';
+const PEER_PREP = 'https://ecme.acep.org/diweb/mylearning';
 
 export default function App() {
   const [isAppReady, setIsAppReady] = useState(false);
+
+  const injectJavascriptCode = `
+    document.addEventListener('click', function(e) {
+      var target = e.target;
+      while (target && target.tagName !== 'A') {
+        target = target.parentElement;
+      }
+      if (target && target.className.includes('launch')) {
+        e.preventDefault();
+        window.ReactNativeWebView.postMessage(target.href);
+      }
+    });
+  `;
+  const webViewRef = useRef<WebView>(null);
+  const onMessage = (event: WebViewMessageEvent) => {
+    const url = event.nativeEvent.data;
+    if (webViewRef.current) {
+      webViewRef.current.injectJavaScript(`window.location.href = '${url}';`); // Redirect to the URL from the clicked link
+    }
+  };
+
   useEffect(() => {
-    setTimeout(() => {
-      
-    }, 2000);
+    setTimeout(() => {}, 2000);
     setIsAppReady(true);
   }, []);
+
   return (
     <WithSplashScreen isAppReady={isAppReady}>
       <View style={styles.container}>
         <View style={{width: '100%', height: '100%'}}>
-          <WebView source={{uri: PEER_PREP}} />
+          <WebView
+            ref={webViewRef}
+            source={{uri: PEER_PREP}}
+            injectedJavaScript={injectJavascriptCode}
+            onMessage={onMessage}
+          />
         </View>
       </View>
     </WithSplashScreen>
